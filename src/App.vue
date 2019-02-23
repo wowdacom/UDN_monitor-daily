069bc02b635e2b0d3233141e2803c3c7fe355f2c
Jim <wowdacom@gmail.com>
2019-02-23 18:43:47 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div class="introduction">
+      
+    </div>
+    <div class="story">
+      <div class="describe">
+
+      </div>
+      <div class="warning">
+
+      </div>
+    </div> -->
+    <div v-if="step === 0">
+      <storyboarder v-hammer:swipe.horizontal="changePage" msg="Welcome to Your Vue.js App"/>
+    </div>
+    <div v-if="step === 1">
+      <storyboarder msg="Tts step 2"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import storyboarder from './components/storyboarder.vue';
 
 export default {
   name: 'app',
+  data () {
+    return {
+      step: 0
+    }
+  },
   components: {
-    HelloWorld
+    storyboarder
+  },
+  methods: {
+    changePage (event) {
+      if (event.type === 'swiperight') {
+        this.step--
+      } else {
+        this.step++
+      }
+      
+      console.log(event)
+    }
   }
 }
 </script>
 
 <style lang="scss">
+// html, body {
+//   overflow: hidden;
+// }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { VueHammer } from 'vue2-hammer'
 
 Vue.config.productionTip = false
+Vue.use(VueHammer)
 
 new Vue({
   store,
